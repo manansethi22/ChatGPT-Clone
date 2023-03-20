821b44e0598ede47e2cd90fd915bbe98715f3d50
Manansethi22 <manansethi2002@gmail.com>
2023-03-20 11:11:31 +0530

    Check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App min-h-screen min-w-full">
       <div className="p-10 text-red-500 text-4xl">Hello World!!</div>
+      <div></div>
     </div>
   );
 }

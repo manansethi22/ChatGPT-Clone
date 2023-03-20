8f061e9a817009edff8a3e3c7b4260e09b05b028
Manansethi22 <manansethi2002@gmail.com>
2023-03-20 11:09:58 +0530

    ChatGPT

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App min-h-screen min-w-full">
+      <div className="p-10 text-red-500 text-4xl">Hello World!!</div>
+    </div>
+  );
+}
+
+export default App;
